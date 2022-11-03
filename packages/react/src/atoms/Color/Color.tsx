2eb04acc9a8e17418b28d1aa4381ffaa12371fbf
Abolfazl-Khan <Abolfazlkhan.m@gmail.com>
2022-11-03 14:13:12 +0330

    extracted foundation to make it a pckage

--- packages/react/src/atoms/Color/Color.tsx
+++ packages/react/src/atoms/Color/Color.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import Spacing from '../../foundation/Spacing';
-interface ColorProps {
+import { Spacing } from '@ds.e/foundation';
+
+export interface ColorProps {
   hexCode: string;
   width?: keyof typeof Spacing;
   height?: keyof typeof Spacing;
 }
 
-function Color({
+const Color: React.FC<ColorProps> = ({
   hexCode,
   width = Spacing.sm,
   height = Spacing.sm,
-}: ColorProps) {
+}) => {
   const className = `dse-width-${width} dse-height-${height}`;
-  return <div className={className} style={{ backgroundColor: hexCode }}></div>;
-}
+
+  return (
+    <div
+      className={className}
+      style={{
+        backgroundColor: hexCode,
+      }}
+    ></div>
+  );
+};
 
 export default Color;
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Color } from '@ds.e/react';
-import '@ds.e/scss/lib/Utilities.css';
+import { Text } from '@ds.e/react';
 
-ReactDOM.render(<Color hexCode='#000' />, document.querySelector('#root'));
+import '@ds.e/scss/lib/Utilities.css';
+import '@ds.e/scss/lib/Text.css';
+import '@ds.e/scss/lib/Margin.css';
+import '@ds.e/scss/lib/Select.css';
+import '@ds.e/scss/lib/global.css';
+
+ReactDOM.render(<Text size='xl'>test</Text>, document.querySelector('#root'));
